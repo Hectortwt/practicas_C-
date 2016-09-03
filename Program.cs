b8e9009c3f650ca56371f317a5dddf3bbf31cd80
Hector R. R <hectortwt@gmail.com>
2016-09-03 18:03:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* introduccion al .NET - framework
+ * Hola Mundo en C#v */
+
+namespace Consola_HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¡Hola mundo!");
+            Console.ReadKey();
+
+        }
+    }
+}
