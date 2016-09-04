9ac120d6d5307bf9d6f269d652166c4a3e842178
Hector R. R <hectortwt@gmail.com>
2016-09-03 19:08:49 -0500

    Update and rename Program.cs to Program02.cs

--- Program02.cs
+++ Program02.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// programa donde se ingresan datos desde consola y después se pregunta su se quiere sumar o restar
+// programa donde se ingresan datos desde consola y después se pregunta si se quiere sumar o restar
 
 namespace ConsoleApp_metodos_ejemplo001
 {
@@ -35,13 +35,13 @@ namespace ConsoleApp_metodos_ejemplo001
             
             // analisis de la decision
 
-            if (opcion == 1) { // si se seleccion la opcion 1 en tonces se sumaran los valores
+            if (opcion == 1) { // si se selecciona la opción 1 en tonces se sumaran los valores
                 resultado = A + B;
                 Console.WriteLine("oK, seleccionaste 1, la suma es:" + resultado);
             }
             else
             {
-                resultado = A - B; // si se seleccion la opcion 2 entonces calcula la resta
+                resultado = A - B; // si se seleccion la opción 2 entonces calcula la resta
                 Console.WriteLine("oK, seleccionaste 2, la suma es:" + resultado);
             }
 
